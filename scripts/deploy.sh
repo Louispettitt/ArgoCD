e915727026d507b79b88e4a39353e0438ac12f6c
Louis Pettitt <louis.pettitt@lifeqisystem.com>
2021-10-29 11:29:13 +0100

    Update deployment spec so it deploys

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,10 @@
 echo "Deploying Keel"
 
 kubectl apply -f /Users/$USER/Repositories/argocd/ArgoCD-1/keel/deployment.yaml
+echo "Please wait 30 seconds for message - Keel deployment complete - so pods have time to be created."
+sleep 30s
+
+echo "Keel deployment complete."
 
 echo "Deploying ArgoCD with test application"
 
@@ -15,9 +19,9 @@ kubectl apply -f /Users/$USER/Repositories/argocd/ArgoCD-1/dev
 kubectl apply -f /Users/$USER/Repositories/argocd/ArgoCD-1/application.yaml
 
 # Allow pods to spin up before completing deployment and beginning login script.
-echo "Please wait 30 seconds for message - Deployment complete - so pods have time to be created."
+echo "Please wait 30 seconds for message - ArgoCD deployment complete - so pods have time to be created."
 sleep 30s
 
-echo "Deployment complete."
+echo "ArgoCD deployment complete."
 
 . /Users/$USER/Repositories/argocd/ArgoCD-1/scripts/login.sh
